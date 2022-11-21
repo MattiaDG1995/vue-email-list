51118532b2acf21211fa14699d7b6c6148ea6d3e
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-21 15:10:09 +0100

    stampo lista

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 var app = new Vue({
     el: '#root',
-    
+    data:{
+        mail:[],
+    },
+
+    methods:{
+
+        start(){
+
+            for(let i=0; i < 10; i++){
+                if( this.mail.length < 10){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                   .then( (res) => {
+            
+                    let newMail = res.data.response
+                    this.mail.push(newMail)
+                    console.log(this.mail)
+                })
+            }
+                
+            }
+        
+            
+            
+
+        }
+
+        
+    }
 
 })
